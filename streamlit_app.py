ca11f573ea9de6cf8b055613315f6a99113f86b6
hanslhansl <>
2025-09-10 22:36:54 +0200

    first steps with streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
 import create_weapon_statistics_file, streamlit as st
+
+@st.cache_data
+def get_weapons_list():
+    return create_weapon_statistics_file.get_weapons_list()
+
+weapons = get_weapons_list()
