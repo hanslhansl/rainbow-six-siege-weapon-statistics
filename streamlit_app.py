5131ada33e4b985731efda32c5be0366c2edbabd
hanslhansl <>
2025-09-11 10:17:20 +0200

    small improvements to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import create_weapon_statistics_file, streamlit as st, matplotlib.pyplot as plt, pandas as pd
 
 # https://demo-stockpeers.streamlit.app/?ref=streamlit-io-gallery-favorites&stocks=AAPL%2CMSFT%2CGOOGL%2CNVDA%2CAMZN%2CTSLA%2CADP%2CACN%2CABBV%2CAMT%2CAXP%2CAMGN%2CAMD
+github_url = "https://github.com/hanslhansl/rainbow-six-siege-weapon-statistics"
 
 @st.cache_data
 def get_weapons_list():
@@ -9,20 +10,22 @@ def get_weapons_list():
 # Calculate average damage per distance across selected weapons
 def calculate_average_damage(selected_weapons : create_weapon_statistics_file.Weapon):
     num_weapons = len(selected_weapons)
-    return (sum(w.damages[i] for w in selected_weapons) / num_weapons for i in range(len(create_weapon_statistics_file.Weapon.distances)))
+    return [sum(w.damages[i] for w in selected_weapons) / num_weapons for i in range(len(create_weapon_statistics_file.Weapon.distances))]
     
 
 # Load data
 weapons = get_weapons_list()
+raw_data = pd.DataFrame({w.name : w.damages for w in weapons}).transpose()
 
 # Streamlit UI
 st.set_page_config(
-    page_title="R6S Weapon Statistics",
+    page_title="r6s weapon stats",
     page_icon=":material/looks_6:", # counter_6 looks_6
     layout="wide",
 )
-st.title("Rainbow Six Siege Weapon Statistics")
-st.markdown("Interaktive Visualisierung der Schadensdaten pro Kugel über verschiedene Distanzen.")
+st.title("rainbow six siege weapon statistics")
+st.markdown("interactive visualisation of damage data over different distances")
+st.markdown(f"find [project on github]({github_url})")
 
 cols = st.columns([1, 3])
 
@@ -32,17 +35,17 @@ top_left_cell = cols[0].container(
 
 with top_left_cell:
     # Suchfeld und Waffenfilter
-    search_query = st.text_input("🔍 Waffe suchen")
+    search_query = st.text_input("🔍 search weapon")
     filtered_weapons = [w for w in weapons if search_query.lower() in w.name.lower()]
     selected_weapons = st.multiselect(
-        label="Waffen auswählen",
+        label="select weapon",
         options=filtered_weapons,
         #default=filtered_weapons,
         format_func=lambda w: w.name
         )
 
     # Durchschnittsanzeige
-    show_average = st.checkbox("Durchschnitt über ausgewählte Waffen anzeigen")
+    show_average = st.checkbox("show average")
 
 right_cell = cols[1].container(
     border=True, height="stretch", vertical_alignment="center"
@@ -59,18 +62,17 @@ with right_cell:
             avg_damage = calculate_average_damage(selected_weapons)
             ax.plot(create_weapon_statistics_file.Weapon.distances, avg_damage, label="Average", linestyle='--', color='black')
 
-        ax.set_xlabel("Distanz (Meter)")
-        ax.set_ylabel("Schaden pro Kugel")
-        ax.set_title("Schadensverlauf über Distanz")
+        ax.set_xlabel("distance (meter)")
+        ax.set_ylabel("damage per bullet")
+        ax.set_title("damage over distance")
         ax.legend()
         st.pyplot(fig)
     else:
-        st.info("Bitte mindestens eine Waffe auswählen.")
+        st.info("selet at least one weapon")
 
 
 """
-## Raw data
+## raw data
 """
 
-df = pd.DataFrame({w.name : w.damages for w in weapons}).transpose()
-df
+raw_data
