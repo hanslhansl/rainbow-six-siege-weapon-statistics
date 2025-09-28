90c13cab911904a244eb8cafa425cd16d908ba27
hanslhansl <>
2025-09-28 16:49:42 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if False:
 st.markdown(
     """
     <style>
-    /* In the second column (td:nth-child(2)), hide every second data row */
+    /* In the second column (td:nth-child(1)), hide every second data row */
     table tr:nth-child(odd) td:nth-child(2) {
         visibility: hidden;  /* hides content but keeps cell width */
     }
