245db2cb8d4cc9a31e1578756f4c27894c69278e
hanslhansl <>
2025-09-28 17:05:21 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,12 +54,12 @@ st.markdown(
     """
     <style>
     /* Keep every third row visible: 1st, 4th, 7th, ... */
-    tbody tr:nth-child(3n - 1) th:nth-child(1) {
+    tbody tr:nth-child(3n - 2) th:nth-child(1) {
         //visibility: visible;
         color: transparent;
     }
     /* Keep every third row visible: 1st, 4th, 7th, ... */
-    tbody tr:nth-child(3n - 2) th:nth-child(1) {
+    tbody tr:nth-child(3n - 3) th:nth-child(1) {
         //visibility: visible;
         color: transparent;
     }
