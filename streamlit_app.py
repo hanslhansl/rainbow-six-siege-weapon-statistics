7ac12ed48451282022223ea660b556c9ecf0e297
hanslhansl <>
2025-09-28 16:55:10 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ st.markdown(
     """
     <style>
     /* In the second column (td:nth-child(2)), hide every second data row */
-    table tr:nth-child(odd) th:nth-child(1) {
+    tbody tr:nth-child(odd) th:nth-child(1) {
         visibility: hidden;  /* hides content but keeps cell width */
     }
     </style>
